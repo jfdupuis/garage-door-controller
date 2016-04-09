8075defb3f6f2e5affc5f8fd37032fd461f5a6df
Jean-Francois Dupuis <jeanfrancois.dupuis@gmail.com>
2016-04-09 10:46:54 -0700

    Clean trailing white space

--- controller.py
+++ controller.py
@@ -16,10 +16,10 @@ from twisted.web.guard import HTTPAuthSessionWrapper, DigestCredentialFactory, B
 
 class HttpPasswordRealm(object):
     implements(portal.IRealm)
- 
+
     def __init__(self, myresource):
         self.myresource = myresource
-    
+
     def requestAvatar(self, user, mind, *interfaces):
         if IResource in interfaces:
             return (IResource, self.myresource, lambda: None)
@@ -39,9 +39,9 @@ class Door(object):
         self.time_to_open = config.get('time_to_open', 10)
         self.open_time = time.time()
         gpio.setup(self.relay_pin, gpio.OUT)
-        gpio.setup(self.state_pin, gpio.IN, pull_up_down=gpio.PUD_UP)        
+        gpio.setup(self.state_pin, gpio.IN, pull_up_down=gpio.PUD_UP)
         gpio.output(self.relay_pin, True)
-        
+
     def get_state(self):
         if gpio.input(self.state_pin) == 0:
             return 'closed'
@@ -57,7 +57,7 @@ class Door(object):
                 return 'closing'
         else:
             return 'open'
-        
+
     def toggle_relay(self):
         state = self.get_state()
         if (state == 'open'):
@@ -69,7 +69,7 @@ class Door(object):
         else:
             self.last_action = None
             self.last_action_time = None
-        
+
         gpio.output(self.relay_pin, False)
         time.sleep(0.2)
         gpio.output(self.relay_pin, True)
@@ -100,7 +100,7 @@ class Controller():
         else:
             self.alert_type = None
             syslog.syslog("No alerts configured")
-            
+
     def status_check(self):
         for door in self.doors:
             new_state = door.get_state()
@@ -130,7 +130,7 @@ class Controller():
                         self.send_pushbullet(title, message)
                 door.open_time = time.time()
                 door.msg_sent = False
-                
+
     def send_email(self, title, message):
         if self.use_smtp:
             syslog.syslog("Sending email message")
@@ -153,7 +153,7 @@ class Controller():
                  "body": message,
              }), {'Authorization': 'Bearer ' + config['access_token'], 'Content-Type': 'application/json'})
         conn.getresponse()
-        
+
 
     def toggle(self, doorId):
         for d in self.doors:
@@ -161,7 +161,7 @@ class Controller():
                 syslog.syslog('%s: toggled' % d.name)
                 d.toggle_relay()
                 return
-        
+
     def get_updates(self, lastupdate):
         updates = []
         for d in self.doors:
@@ -174,7 +174,7 @@ class Controller():
         root = File('www')
         root.putChild('upd', self.updateHandler)
         root.putChild('cfg', ConfigHandler(self))
-        
+
         clk = ClickHandler(self)
         args={self.config['site']['username']:self.config['site']['password']}
         checker = checkers.InMemoryUsernamePasswordDatabaseDontUse(**args)
@@ -190,11 +190,11 @@ class Controller():
 
 class ClickHandler(Resource):
     isLeaf = True
-    
+
     def __init__ (self, controller):
         Resource.__init__(self)
         self.controller = controller
-    
+
     def render(self, request):
         door = request.args['id'][0]
         self.controller.toggle(door)
@@ -205,13 +205,13 @@ class ConfigHandler(Resource):
     def __init__ (self, controller):
         Resource.__init__(self)
         self.controller = controller
-    
+
     def render(self, request):
         request.setHeader('Content-Type', 'application/json')
-        
+
         return json.dumps([(d.id, d.name, d.last_state, d.last_state_time)
-                            for d in controller.doors])         
-        
+                            for d in controller.doors])
+
 
 class UpdateHandler(Resource):
     isLeaf = True
@@ -219,54 +219,54 @@ class UpdateHandler(Resource):
         Resource.__init__(self)
         self.delayed_requests = []
         self.controller = controller
-    
+
     def handle_updates(self):
         for request in self.delayed_requests:
             updates = self.controller.get_updates(request.lastupdate)
             if updates != []:
                 self.send_updates(request, updates)
                 self.delayed_requests.remove(request);
-    
+
     def format_updates(self, request, update):
         response = json.dumps({'timestamp': int(time.time()), 'update':update})
         if hasattr(request, 'jsonpcallback'):
             return request.jsonpcallback +'('+response+')'
         else:
             return response
-            
+
     def send_updates(self, request, updates):
         request.write(self.format_updates(request, updates))
         request.finish()
-    
+
     def render(self, request):
-        
+
         # set the request content type
         request.setHeader('Content-Type', 'application/json')
-        
+
         # set args
         args = request.args
-       
+
         # set jsonp callback handler name if it exists
         if 'callback' in args:
             request.jsonpcallback =  args['callback'][0]
-           
+
         # set lastupdate if it exists
         if 'lastupdate' in args:
             request.lastupdate = float(args['lastupdate'][0])
         else:
             request.lastupdate = 0
-            
-            #print "request received " + str(request.lastupdate)    
-            
+
+            #print "request received " + str(request.lastupdate)
+
         # Can we accommodate this request now?
         updates = controller.get_updates(request.lastupdate)
         if updates != []:
             return self.format_updates(request, updates)
-        
-        
+
+
         request.notifyFinish().addErrback(lambda x: self.delayed_requests.remove(request))
         self.delayed_requests.append(request)
-        
+
         # tell the client we're not done yet
         return server.NOT_DONE_YET
 
@@ -276,7 +276,7 @@ def elapsed_time(seconds, suffixes=['y','w','d','h','m','s'], add_s=False, separ
     """
     # the formatted time string to be returned
     time = []
-    
+
     # the pieces of time to iterate over (days, hours, minutes, etc)
     # - the first piece in each tuple is the suffix (d, h, w)
     # - the second piece is the length in seconds (a day is 60s * 60m * 24h)
@@ -286,7 +286,7 @@ def elapsed_time(seconds, suffixes=['y','w','d','h','m','s'], add_s=False, separ
              (suffixes[3], 60 * 60),
              (suffixes[4], 60),
              (suffixes[5], 1)]
-    
+
     # for each time piece, grab the value and remaining seconds, and add it to
     # the time string
     for suffix, length in parts:
@@ -299,12 +299,12 @@ def elapsed_time(seconds, suffixes=['y','w','d','h','m','s'], add_s=False, separ
             break
 
     return separator.join(time)
-     
+
 if __name__ == '__main__':
     syslog.openlog('garage_controller')
     config_file = open('config.json')
     controller = Controller(json.load(config_file))
     config_file.close()
     controller.run()
-              
-    
+
+
